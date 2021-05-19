b3b9d09d6f84beb3b8e7b3e3ec41d8c6df4b1916
Willian156 <willian156@outlook.com>
2021-05-19 15:11:00 -0300

    Foi implementado dois botões na tela de ler os diários, um, chamado de "Voltar" que serve para fechar a tela de ler e outro, chamado de "Imprimir", que cria um arquivo .txt em uma pasta específica do computador para poder imprimir o relatório em papel.

--- src/main/java/controller/Diarios_lerController.java
+++ src/main/java/controller/Diarios_lerController.java
@@ -2,8 +2,16 @@ package controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.awt.event.ActionEvent;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,6 +25,33 @@ public class Diarios_lerController implements Initializable {
     TextField txtCriador;
     @FXML
     TextField txtDia;
+    @FXML
+    Button btnVoltar,
+            btnImprimir;
+
+    public void Voltar() throws IOException {
+        Stage stage = (Stage)btnVoltar.getScene().getWindow();
+        stage.close();
+    }
+
+    public void Imprimir() throws IOException {
+
+        FileWriter dro = new FileWriter("d:\\Diarios_impressos\\DiarioID_"+ diarioSelecionado.getId()+ ".txt");
+        PrintWriter gravarDro = new PrintWriter(dro);
+
+        gravarDro.println("Diário realizado no dia "+ diarioSelecionado.getData()+"\n");
+        gravarDro.println("Criado por: "+ diarioSelecionado.getCriador() + "\n");
+        gravarDro.println("\nDescrição:\n"+ diarioSelecionado.getDescricao());
+
+        dro.close();
+
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Arquivo salvo!");
+        alert.setContentText("Arquivo salvo do disco 'D' com o nome de 'DiarioID_"+diarioSelecionado.getId() +"'");
+        alert.show();
+    }
+
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
